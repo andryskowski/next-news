6ae12eccb4075f0464a24d50bbfe713eb0f88b49
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-02-28 11:50:49 +0100

    refactoring

--- components/toolbar.js
+++ components/toolbar.js
@@ -8,7 +8,7 @@ export const Toolbar = () => {
         <div className={styles.main}>
             <div onClick={() => router.push('/')}>Home</div>
             <div onClick={() => router.push('/feed/1')}>Feed</div>
-            <div onClick={() => router.push('/eom')}>Eom</div>
+            <div onClick={() => router.push('/author')}>Author</div>
             {/* <div onClick={() => window.location.href = 'https:://twitter.com'}>Twitter</div> */}
         </div>
     );
--- pages/author.js
+++ pages/author.js
@@ -1,7 +1,7 @@
-import styles from '../styles/EOM.module.css';
+import styles from '../styles/Author.module.css';
 import {Toolbar} from '../components/toolbar';
 
-export const EOM = ({Author}) => {
+export const Author = ({Author}) => {
     return(
         <div className='page-container'>
             <Toolbar></Toolbar>
@@ -10,7 +10,7 @@ export const EOM = ({Author}) => {
                 <div className={styles.author}>
                     <h3>{Author.name}</h3>
                     <h6>{Author.position}</h6>
-                    <img src={author.image}/>
+                    <img src={Author.image}/>
                     <p>{Author.description}</p>
                 </div>
             </div>
@@ -22,7 +22,7 @@ export const getServerSideProps = async pageContext => {
     const apiResponse = await fetch(
         // https://my-json-server.typicode.com/andryskowski/next-news/db
         //http://my-json-server.typicode.com/portexe/next-news/AuthorOfTheMonth
-        `https://my-json-server.typicode.com/andryskowski/next-news/Author`,
+        `https://my-json-server.typicode.com/andryskowski/next-news/author`,
     );
     const Author = await apiResponse.json();
 
@@ -33,4 +33,4 @@ export const getServerSideProps = async pageContext => {
     }
 };
 
-export default EOM;
+export default Author;
